5514f2a62726f128fb1c974bea62992023b0d855
drhanum <hyenums@gmail.com>
2026-05-20 10:40:05 +0700

    fix error

--- src/sections/user/view/user-view.tsx
+++ src/sections/user/view/user-view.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable perfectionist/sort-imports */
 import { useState, useCallback } from 'react';
 
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
